a25ed9ff87a9b423f2cb71edb6392bac5325efcb
5sztuk <milena.renusz@gmail.com>
2019-10-22 20:27:32 +0200

    przyklady z zajec

---
+++ day02.py
@@ -0,0 +1,25 @@
+#x = input("Podaj pierwszą liczbę: ")
+#y = input("Podaj drugą liczbę: ")
+
+#suma = int(x) + int(y)
+
+#print ("1" + "4")
+
+#print(suma)
+
+#tekst = "Ala ma kota"
+#ulamek = 1.34
+#ilosc_powtorzen = 4
+
+#print (ilosc_powtorzen * tekst)
+
+#tekst = "ala ma kota"
+#
+# print (tekst.capitalize())
+
+#print(len(tekst))
+#print(tekst[0])
+
+tekst = "Tytuł: Ala ma kota"
+dlugosc = len(tekst)
+print(tekst[dlugosc-1])
