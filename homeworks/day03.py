993646e30bf39a4e3d2ee3776038c3623f633a8a
5sztuk <milena.renusz@gmail.com>
2019-10-24 20:36:47 +0200

    przyklady z zajec

---
+++ homeworks/day03.py
@@ -0,0 +1,133 @@
+#wiek = input ("ile masz lat")
+#zwierzaki = input ("ile masz kotów")
+
+# wiek = 2
+# zwierzaki = 5
+
+#zdanie = "ala ma 2 lata i posiada 5 kotów"
+
+#zdanie = "Ala ma " + str(wiek) + " lata i posiada " + str(zwierzaki) + " kotów"
+#zdanie = f"Ala ma  {wiek}   lata i posiada   {zwierzaki}  kotów"
+#zdanie = "Ala ma  {}  lata i posiada  {}  kotów".format(wiek, zwierzaki)
+#zdanie = "Ala ma  {a}   lata i posiada {b}  kotów".format(b=wiek, a=zwierzaki)
+
+# liczba = 1.234
+#
+# print ("liczba: %s" % liczba)
+# print ("liczba: %f" % liczba)
+# print ("liczba: %d" % liczba)
+
+#zdanie = "encyklopedia"
+
+# print(zdanie[4])
+# print(zdanie[-3])
+# print(zdanie[2:8])
+# print(zdanie[:7])
+# print(zdanie[8:])
+# print(zdanie[1:7:2])
+# print(zdanie[::-1])
+
+# print("start")
+# if True:
+#         print("prawda")
+# print("koniec")
+
+# print ("start")
+# if "1" == "1":
+#     print("prawda")
+# else
+#     print("Bzdura")
+
+# print("spr parzystosci liczby")
+# liczba = int(input("podaj liczbę"))
+# if liczba % 2 == 0:
+#     print("liczba jest parzysta")
+# else:
+#     print("liczba nieparzysta")
+
+# print("spr podzielności liczby")
+# liczba = int(input("podaj liczbę"))
+# if liczba % 3 == 0 and liczba % 5 == 0:
+#     print("liczba jest podzielna przez 3 i 5")
+# if liczba % 3 == 0:
+#     print("liczba jest podzielna przez 3")
+# elif liczba % 5 == 0:
+#     print("liczba jest podzielna przez 5")
+# else:
+#     print("liczba jest niepodzielna przez 3 i 5")
+
+
+# wyrazy = ("raz", "dwa")
+# wyrazy [0] = "jeden"
+# print(wyrazy)
+
+
+# liczby_parzyste = range (0,20,2)
+# if 10 in liczby_parzyste:
+#     print("prawda")
+#
+#
+# lista_liczb_parzystych = list(range (0,20,2))
+# print(lista_liczb_parzystych)
+#
+# lista_liczb_parzystych = tuple(range (0,20,2))
+# print(lista_liczb_parzystych)
+
+# napis = "dwa"
+# lista = list("napis")
+# print(lista)
+# lista[0]='a'
+# print(lista)
+
+# zakupy = ["kielbasa", "piwko", "chipsy", "węgiel", "kubeczki"]
+# print(zakupy)
+# zakupy.append("talerzyki")
+# print(zakupy)
+# zakupy.insert(0, "grill")
+# print(zakupy)
+# zakupy[0] = "elektryczny grill"
+# print(zakupy)
+# zakupy.remove("piwko")
+# del(zakupy[3])
+# print(zakupy)
+
+# lista = [[1,2,3], [4,5,6], [7,8,9]]
+# lista = (
+#         [1,2,3],
+#         [4,5,6],
+#         [7,8,9]
+# )
+# lista[1][2]
+# print(lista[1][2])
+
+
+# print("start")
+# zapytaj_ponownie = "T"
+# while zapytaj_ponownie =="T":
+#     zapytaj_ponownie = input("czy zapytać ponownie? [T/N]").upper()
+#     print("odpowiedziałeś: " +zapytaj_ponownie)
+# print("koniec")
+
+
+# zakupy = ["kielbasa", "piwko", "chipsy", "węgiel", "kubeczki"]
+# for przedmiot in zakupy:
+#     if przedmiot == 'piwko':
+#         znak = '[x] '
+#     else:
+#         znak = '[ ]  '
+#     print(znak + przedmiot)
+
+
+for litera in "jakies zdanie":
+    print(litera)
+
+
+
+
+
+
+
+
+
+
+
